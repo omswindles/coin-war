f03a93236d53d973724a61b9be5c8d36861be37c
omswindles <edub629@gmail.com>
2014-03-14 15:23:54 -0700

    cleaned up interface messages, added check for army size in war_prep()

--- coin-war.py
+++ coin-war.py
@@ -5,7 +5,7 @@
 
 import random
 import time
-from sys import exit
+
 
 
 def generate_army():
@@ -54,7 +54,7 @@ def battle(player1, player2):
         army1 = player1 + prisoners2 + prisoners1
         prison1 = []
         prison2 = []
-        return "player 1 wins"
+        return "Player 1 won the battle."
     if player2[0] < player1[0]:
         player2 += player1[0] + player2[0]
         del player1[0]
@@ -62,21 +62,21 @@ def battle(player1, player2):
         army2 = player2 + prisoners1 + prisoners2
         prison1 = []
         prison2 = []
-        return "player 2 wins"
+        return "Player 2 won the battle."
     if player1[0] == player2[0]:
         prison1 += player1[0:2]
         prison2 += player2[0:2]
         del player1[0:2]
         del player2[0:2]
-        return "tied"
+        return "Battle was a draw."
 
 
 def battle_check():
     if not army1:
-        #print("Player 2 wins, Player 1 ran out of troops.")
+        print("Player 1 has no army. Player 2 has won the war.")
         return False
     if not army2:
-        #print("Player 1 wins, Player 2 ran out of troops.")
+        print("Player 2 has no army. Player 1 has won the war.")
         return False
     if not army1 and army2:
         prison_count1 = 0
@@ -88,13 +88,13 @@ def battle_check():
             if prison2[j] == 1:
                 prison_count2 += 1
         if prison_count1 == prison_count2:
-            print("No armies, Tie game")
+            print("No armies, the war is a DRAW?!?")
             return False
         elif prison_count1 > prison_count2:
-            print("Player 1 wins!")
+            print("Player 1 wins! Player 2 has a lesser prisoner count.")
             return False
         elif prison_count2 > prison_count1:
-            print("Player 2 Wins!")
+            print("Player 2 Wins! Player 1 has a lesser prisoner count.")
             return False
     else:
         return True
@@ -111,14 +111,18 @@ def war():
         display_(2)
         time.sleep(1)
         print("Result of Round " + str(current_round) + ": " + battle(army1, army2) + ".")
+        print()
         current_round += 1
         
     else:
+        print()
         print("GAME OVER!")
         print("Player 1: ", end="")
         display_(1)
         print("Player 2: ", end="")
         display_(2)
+        print()
+        battle_check()
     return True
 
 
@@ -127,7 +131,7 @@ def war_prep():
     global army2
     troops1 = []
     troops2 = []
-    print("Prepare for WAR!!")
+    print("Prepare for WAR!! Pick 5 troops must be 5 units long and contain either H's T's or both.")
     troops1 += str(input("Player 1 troop selection? "))
     troops2 += str(input("Player 2 troop selection? "))
     
@@ -135,34 +139,37 @@ def war_prep():
         troops1 = generate_army()
     if not troops2:
         troops2 = generate_army()
+    if len(troops1) != 5 or len(troops2) != 5:
+        print("Army Selection Error: Each army must have 5 units or be left blank for random selection.")
+        war_prep()
         
-    index = 0 # initialize troop index for first troop conversion
-    for current_troop in troops1: #convert troops from h or H to 1 and t or T to 2
+    index = 0  # initialize troop index for first troop conversion
+    for current_troop in troops1:
+        # convert troops from h or H to 1
         if current_troop == "H" or current_troop == "h":
             troops1[index] = "1"
+        # convert troops from t or T to 2
         if current_troop == "T" or current_troop == "t":
             troops1[index] = "2"
         index += 1
 
-    index = 0 # re-initialize troop index for second troop conversion
+    index = 0  # re-initialize troop index for second troop conversion
     for current_troop in troops2:
-        #convert troops from h or H to 1
+        # convert troops from h or H to 1
         if current_troop == "H" or current_troop == "h":
             troops2[index] = "1"
-        #convert troops from t or T to 2
+        # convert troops from t or T to 2
         if current_troop == "T" or current_troop == "t":
             troops2[index] = "2"
             
-        index += 1 # increments index
+        index += 1  # increments index
             
     army1 = troops1
     army2 = troops2
 
-    print(troops1)
-    print(troops2)
-    print("ARMY1: ", end="")
+    print("Army 1 Selection: ", end="")
     display_(1)
-    print("ARMY2: ", end="")
+    print("Army 2 Selection: ", end="")
     display_(2)
 
 
@@ -171,6 +178,6 @@ army1 = []
 army2 = []
 prison1 = []
 prison2 = []
-current_round = 1 # initializes round counter
+current_round = 1  # initializes round counter
 war_prep()
 war()
